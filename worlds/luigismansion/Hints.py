39181be563615423b11c205fdb0d1836eee6c650
BootsinSoots <theron.swanson@gmail.com>
2025-07-23 13:01:31 -0400

    Make sure we don't error when passing hints through

--- worlds/luigismansion/Hints.py
+++ worlds/luigismansion/Hints.py
@@ -84,10 +84,10 @@ def get_hints_by_option(multiworld: MultiWorld, player_hints: set[int]) -> None:
                     icolor = "Other"
                 hint = {name: {"Item": loc.item.name,
                                "Location": loc.name,
-                               "Location ID": loc.address,
+                               "Location ID": str(loc.address),
                                "Rec Player": multiworld.player_name[loc.item.player],
                                "Send Player": multiworld.player_name[loc.player],
-                               "Send Player ID": loc.player,
+                               "Send Player ID": str(loc.player),
                                "Game": loc.game,
                                "Class": icolor}}
                 already_hinted_locations.append(loc)
