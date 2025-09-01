2237ccef39f6cf177902510024285b56c8985a42
SomeJakeGuy <somejakeguy@gmail.com>
2025-09-01 16:13:38 -0400

    Moved LM Client's dolphin connection constants to the client constants file.

--- worlds/luigismansion/client/constants.py
+++ worlds/luigismansion/client/constants.py
@@ -1,3 +1,11 @@
 """ Collection of commonly used constants for Luigi's Mansion. """
 
 CLIENT_VERSION = "V0.5.5"
+
+# All the dolphin connection messages used in the client
+CONNECTION_REFUSED_STATUS = "Detected a non-randomized ROM for LM. Please close and load a different one. Retrying in 5 seconds..."
+CONNECTION_LOST_STATUS = "Dolphin connection was lost. Please restart your emulator and make sure LM is running."
+NO_SLOT_NAME_STATUS = "No slot name was detected. Ensure a randomized ROM is loaded. Retrying in 5 seconds..."
+CONNECTION_VERIFY_SERVER = "Dolphin was confirmed to be opened and ready, Connect to the server when ready..."
+CONNECTION_INITIAL_STATUS = "Dolphin emulator was not detected to be running. Retrying in 5 seconds..."
+CONNECTION_CONNECTED_STATUS = "Dolphin is connected, AP is connected, Ready to play LM!"
