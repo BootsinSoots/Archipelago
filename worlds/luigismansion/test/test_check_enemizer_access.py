26f7285168800cd40b69589986156f24a02e679e
SomeJakeGuy <somejakeguy@gmail.com>
2025-09-11 09:54:05 -0400

    Fixed our updated world test for accessing enemizer locations. Currently only checks 1F bathroom, can be updated to check all.

--- worlds/luigismansion/test/test_check_enemizer_access.py
+++ worlds/luigismansion/test/test_check_enemizer_access.py
@@ -6,6 +6,7 @@ from .. import LMWorld, ITEM_TABLE
 class LMTestBase(WorldTestBase):
     game = "Luigi's Mansion"
     world = LMWorld
+    run_default_tests = False
 
 class LocationFail(LMTestBase):
     options = {
@@ -13,12 +14,16 @@ class LocationFail(LMTestBase):
     }
 
     def test_enemizer_locations_requires_element(self):
+        if self.world.ghost_affected_regions["1F Bathroom"] == "No Element":
+            print("1F Bathroom did not require an element, exiting...")
+            return
+
         prog_items = []
         for name, data in ITEM_TABLE.items():
             if data.classification == ItemClassification.progression and not data.type == "Medal":
                 prog_items.append(name)
 
         self.collect_by_name(prog_items)
-        print(self.multiworld.state.prog_items)
-        print(self.multiworld.seed)
+        print("Currently all of the multiworld's progressive items required are: " + str(self.multiworld.state.prog_items))
+        print("Multiworld Seed was: " + self.multiworld.seed)
         self.assertFalse(self.multiworld.state.can_reach_location("1F Bathroom Shelf Key", self.player))
