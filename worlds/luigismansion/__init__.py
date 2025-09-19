7b65f78641b52b9da0df52ae0f940d78b143f989
BootsinSoots <theron.swanson@gmail.com>
2025-09-18 22:00:09 -0400

    Attempt to fix non-local everything/early key, again

--- worlds/luigismansion/__init__.py
+++ worlds/luigismansion/__init__.py
@@ -542,7 +542,10 @@ class LMWorld(World):
             self.options.balcony_boo_count.value = 0
             # self.options.washroom_boo_count.value = 0
 
-    def create_regions(self):
+        self.lm_create_regions()
+        self.lm_create_items()
+
+    def lm_create_regions(self):
         # Add all randomizable regions
         for region_name in REGION_LIST.values():
             self.multiworld.regions.append(Region(region_name, self.player, self.multiworld))
@@ -595,7 +598,7 @@ class LMWorld(World):
     # def post_fill(self):
     #     visualize_regions(self.multiworld.get_region(self.origin_region_name, self.player), "luigiregions.puml", linetype_ortho=False)
 
-    def create_items(self):
+    def lm_create_items(self):
         exclude = [item.name for item in self.multiworld.precollected_items[self.player]]
         loc_itempool: list[LMItem] = []
         if self.options.boosanity:
