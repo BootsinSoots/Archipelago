fd1b121775196212d25a3e1f6f81fce485774f40
SomeJakeGuy <39894486+SomeJakeGuy@users.noreply.github.com>
2025-09-19 01:02:54 -0400

    Update constants.py

--- worlds/luigismansion/client/constants.py
+++ worlds/luigismansion/client/constants.py
@@ -1,6 +1,6 @@
 """ Collection of commonly used constants for Luigi's Mansion. """
 
-CLIENT_VERSION = "V0.5.5"
+CLIENT_VERSION = "V0.5.6"
 CLIENT_NAME = "Luigi's Mansion Client"
 
 AP_LOGGER_NAME = "Client"
