b1ad0139f77dfa00dd3f7a66803c2d7bd4623732
SomeJakeGuy <somejakeguy@gmail.com>
2025-10-19 19:32:13 -0400

    Replaced private variable from double _ to single

--- worlds/luigismansion/iso_helper/JMP_Field_Header.py
+++ worlds/luigismansion/iso_helper/JMP_Field_Header.py
@@ -4,11 +4,11 @@ from io import BytesIO
 IMPORTANT_HEADER_BYTE_LENGTH = 12
 
 class JMPFieldHeader:
-    __field_name = ""
-    __field_hash = 0
-    __field_bitmask = 0
-    __field_shift_bit = 0
-    __field_data_type = ""
+    _field_name = ""
+    _field_hash = 0
+    _field_bitmask = 0
+    _field_shift_bit = 0
+    _field_data_type = ""
 
     # First 4 bytes represent the field's hash.
     # The second 4 bytes represent the field bitmask, which is used for modifying byte data. It can help
@@ -17,40 +17,40 @@ class JMPFieldHeader:
     # The second to last byte represents the shift bits, which is required when reading certain field data.
     # The last byte represents the data type, which for this project's purposes include:
     #   Integer, String, and Float.
-    def __init__(self, name: str, header_bytes: BytesIO):
-        self.__field_name = name
-        self.__field_hash, self.__field_bitmask, self.__field_start_bit, self.__field_shift_bit, data_type = (
+    def _init_(self, name: str, header_bytes: BytesIO):
+        self._field_name = name
+        self._field_hash, self._field_bitmask, self._field_start_bit, self._field_shift_bit, data_type = (
             struct.unpack(">I I H B B", header_bytes.read(IMPORTANT_HEADER_BYTE_LENGTH)))
         match data_type:
             case 0:
-                self.__field_data_type="Int"
+                self._field_data_type="Int"
             case 1:
-                self.__field_data_type="Str"
+                self._field_data_type="Str"
             case 2:
-                self.__field_data_type = "Flt"
+                self._field_data_type = "Flt"
             case _:
                 raise Exception("Unknown Data Type provided: " + str(data_type))
 
     @property
     def get_field_type(self):
-        return self.__field_data_type
+        return self._field_data_type
 
     @property
     def get_field_name(self):
-        return self.__field_name
+        return self._field_name
 
     @property
     def get_field_hash(self):
-        return self.__field_hash
+        return self._field_hash
 
     @property
     def get_field_bitmask(self):
-        return self.__field_bitmask
+        return self._field_bitmask
 
     @property
     def get_field_start_bit(self):
-        return self.__field_start_bit
+        return self._field_start_bit
 
     @property
     def get_field_shift_bit(self):
-        return self.__field_shift_bit
+        return self._field_shift_bit
