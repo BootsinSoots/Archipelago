4e262c8c55540b832aeb46a54a29472df11c4d9b
BootsinSoots <theron.swanson@gmail.com>
2025-10-20 00:10:07 -0400

    Convert to REGION_LIST in LM Client

--- worlds/luigismansion/LMClient.py
+++ worlds/luigismansion/LMClient.py
@@ -9,6 +9,7 @@ from CommonClient import get_base_parser, gui_enabled, server_loop
 # 3rd Party related imports
 import dolphin_memory_engine as dme
 
+from worlds.luigismansion import REGION_LIST
 # Local related imports
 from .client.contexts.base_context import BaseContext, logger
 from .Regions import spawn_locations
@@ -440,7 +441,7 @@ class LMContext(BaseContext):
                 if current_map_id == 2:
                     # If special moving Toad, room_to_check should be the spawn room id
                     if lm_loc_data.code == 617:
-                        room_to_check: int = spawn_locations[self.spawn]["in_game_room_id"]
+                        room_to_check: int = REGION_LIST[self.spawn].in_game_room_id
                     else:
                         room_to_check = loc_addr.in_game_room_id if not loc_addr.in_game_room_id is None else current_room_id
                     if not room_to_check == current_room_id:
