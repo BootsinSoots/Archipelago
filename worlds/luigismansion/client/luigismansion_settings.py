2a02fa1076d8b4bba1e25ad2e43b9f5ac7c69112
SomeJakeGuy <somejakeguy@gmail.com>
2026-03-06 04:37:28 -0500

    [Settings] Create a yaml constructor for EmulatorSettings. Introduce Dolphin Process Name.

--- worlds/luigismansion/client/luigismansion_settings.py
+++ worlds/luigismansion/client/luigismansion_settings.py
@@ -1,4 +1,5 @@
 import settings
+import yaml
 
 class EmulatorExecutable(settings.UserFilePath):
     """
@@ -6,16 +7,26 @@ class EmulatorExecutable(settings.UserFilePath):
     If using Flatpak, specify the path here.
     """
     is_exe = True
-    description = "The path for emulator executable. If using Flatpak specify this path instead."
+    description = "The path for emulator executable. If using Flatpak, specify this path instead."
 
 class EmulatorAdditionalArguments(list[str]):
     """ Additional arugments to be passed in when auto starting emulator. """
     args = []
 
+def emulator_settings_constructor(loader, node):
+    _ = loader.construct_mapping(node, deep=True)
+    return EmulatorSettings()
+
 class EmulatorSettings(settings.Group):
+    """Various Emulator specific settings (such as Dolphin)"""
     path: EmulatorExecutable = EmulatorExecutable()
     additional_args: EmulatorAdditionalArguments = EmulatorAdditionalArguments([ ])
     auto_start: bool = True
+
+    yaml.add_constructor(
+        'tag:yaml.org,2002:python/object:worlds.luigismansion.client.luigismansion_settings.EmulatorSettings',
+        emulator_settings_constructor
+    )
 
 class ISOFile(settings.UserFilePath):
     """ Locate your Luigi's Mansion ISO """
@@ -23,6 +34,11 @@ class ISOFile(settings.UserFilePath):
     copy_to = None
     md5s = ["6e3d9ae0ed2fbd2f77fa1ca09a60c494"]
 
+class DolphinProcessName(str):
+    """The name of the Dolphin process to connect to. Leave blank for system default."""
+
 class LuigisMansionSettings(settings.Group):
+    """Various Luigi Mansion Settings"""
     iso_file: ISOFile = ISOFile(ISOFile.copy_to)
     dolphin_settings: EmulatorSettings = EmulatorSettings()
+    dolphin_process_name: DolphinProcessName = ""
