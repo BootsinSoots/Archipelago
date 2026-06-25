ce1fd97353a1743495d5918946a7c0801444c881
BootsinSoots <theron.swanson@gmail.com>
2026-06-25 15:15:14 -0400

    [Constants] Add Item and Location name constant files

---
+++ worlds/smgalaxy2/Constants/Names/item_names.py
@@ -0,0 +1,8 @@
+# Keyed Grand Stars
+GRAND1 = "Grand Star - World 1"
+GRAND2 = "Grand Star - World 2"
+GRAND3 = "Grand Star - World 3"
+GRAND4 = "Grand Star - World 4"
+GRAND5 = "Grand Star - World 5"
+GRAND6 = "Grand Star - World 6"
+GRAND7 = "Grand Star - World 7"
