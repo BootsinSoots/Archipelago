417f32b7bfe5b3595a4cee3cfabe1ca84e9f76ea
BootsinSoots <theron.swanson@gmail.com>
2026-06-30 01:41:09 -0400

    [Locations] Add all Star location names, thanks to  Mystic

--- worlds/smgalaxy2/Constants/Names/location_names.py
+++ worlds/smgalaxy2/Constants/Names/location_names.py
@@ -6,7 +6,41 @@ SKYSTASTARG1 = "Sky Station: Green Star 1"
 SKYSTASTARG2 = "Sky Station: Green Star 2"
 SKYSTASTARG3 = "Sky Station: Green Star 3"
 
-# Yoshi Star Galaxy
+# Yoshi Star Star Locations
+YOSHSTARSTAR1 = "Yoshi Star 1: Saddle Up With Yoshi"
+YOSHSTARSTAR2 = "Yoshi Star 2: Spiny Control"
+YOSHSTARSTAR3 = "Yoshi Star 3: Spiny Rainbow Romp"
+YOSHSTARSTARG1 = "Yoshi Star: Green Star 1"
+YOSHSTARSTARG2 = "Yoshi Star: Green Star 2"
+YOSHSTARSTARG3 = "Yoshi Star: Green Star 3"
+
+# Spin-Dig Star Locations
+SPINDIGSTAR1 = "Spin-Dig 1: Digga-Leg's Planet"
+SPINDIGSTAR2 = "Spin-Dig 2: Silver Stars Down Deep"
+SPINDIGSTAR3 = "Spin-Dig 3: Digga-Leg's Daredevil Run"
+SPINDIGSTARG1 = "Spin-Dig: Green Star 1"
+SPINDIGSTARG2 = "Spin-Dig: Green Star 2"
+SPINDIGSTARG3 = "Spin-Dig: Green Star 3"
+
+# Fluffy Bluff Star Locations
+FLUFBLUFSTAR1 = "Fluffy Bluff 1: Search for the Toad Brigade Captain"
+FLUFBLUFSTAR2 = "Fluffy Bluff 2: The Chimp's Stomp Challenge"
+FLUFBLUFSTAR3 = "Fluffy Bluff 3: Every Planet Has Its Price"
+FLUFBLUFSTARG1 = "Fluffy Bluff: Green Star 1"
+FLUFBLUFSTARG2 = "Fluffy Bluff: Green Star 2"
+FLUFBLUFSTARG3 = "Fluffy Bluff: Green Star 3"
+
+# Rightside Down Star Locations
+RIGHTDOWNSTAR1 = "Rightside Down 1: Breaking the Laws of Gravity"
+RIGHTDOWNSTAR2 = "Rightside Down 2: The Great Crate Incinerator"
+RIGHTDOWNSTARG1 = "Rightside Down: Green Star 1"
+RIGHTDOWNSTARG2 = "Rightside Down: Green Star 2"
+
+# Flip-Swap Star Locations
+FLIPSWAPSTAR1 = "Flip-Swap 1: Think Before You Shake"
+FLIPSWAPSTAR2 = "Flip-Swap 2: Purple Coin Flip 'n' Sprint"
+FLIPSWAPSTARG1 = "Flip-Swap: Green Star 1"
+FLIPSWAPSTARG2 = "Flip-Swap: Green Star 2"
 
 # Bowser Jr.s Fiery Flotilla
 FIREFLOTSTAR1 = "Fiery Flotilla 1: Gobblegut's Aching Belly"
@@ -14,20 +48,297 @@ FIREFLOTSTAR2 = "Fiery Flotilla 2: Fiery Flotilla Speed Run"
 FIREFLOTSTARG1 = "Fiery Flotilla: Green Star 1"
 FIREFLOTSTARG2 = "Fiery Flotilla: Green Star 2"
 
-# Bowser's Lava Lair
+# World 2
+# Puzzle Plank Star Locations
+PUZZPLANKSTAR1 = "Puzzle Plank 1: The Puzzling Picture Block"
+PUZZPLANKSTAR2 = "Puzzle Plank 2: Purple Coin Shadow Vault"
+PUZZPLANKSTAR3 = "Puzzle Plank 3: Bugaboom's Back"
+PUZZPLANKSTARG1 = "Puzzle Plank: Green Star 1"
+PUZZPLANKSTARG2 = "Puzzle Plank: Green Star 2"
+PUZZPLANKSTARG3 = "Puzzle Plank: Green Star 3"
+
+# Hightail Falls Star Locations
+HIGHFALLSTAR1 = "Hightail Falls 1: Hot-Stepping Dash Pepper"
+HIGHFALLSTAR2 = "Hightail Falls 2: Hightail Falls Speed Run"
+HIGHFALLSTAR3 = "Hightail Falls 3: Silver Stars in Hightail Falls"
+HIGHFALLSTARG1 = "Hightail Falls: Green Star 1"
+HIGHFALLSTARG2 = "Hightail Falls: Green Star 2"
+HIGHFALLSTARG3 = "Hightail Falls: Green Star 3"
+
+# Boulder Bowl Star Locations
+ROCKBOWLSTAR1 = "Boulder Bowl 1: Rock and Rollodillo"
+ROCKBOWLSTAR2 = "Boulder Bowl 2: Rolling Crabber Romp"
+ROCKBOWLSTAR3 = "Boulder Bowl 3: C'mere, Goomba"
+ROCKBOWLSTARG1 = "Boulder Bowl: Green Star 1"
+ROCKBOWLSTARG2 = "Boulder Bowl: Green Star 2"
+ROCKBOWLSTARG3 = "Boulder Bowl: Green Star 3"
+
+# Cosmic Cove Star Locations
+COSCCOVESTAR1 = "Cosmic Cove 1: Twin Falls Hideaway"
+COSCCOVESTAR2 = "Cosmic Cove 2: Exploring the Cosmic Cavern"
+COSCCOVESTAR3 = "Cosmic Cove 3: Catch That Star Bunny"
+COSCCOVESTARG1 = "Cosmic Cove: Green Star 1"
+COSCCOVESTARG2 = "Cosmic Cove: Green Star 2"
+COSCCOVESTARG3 = "Cosmic Cove: Green Star 3"
+
+# Wild Glide Star Locations
+WILDGLIDESTAR1 = "Wild Glide 1: Fluzzard's First Flight"
+WILDGLIDESTAR2 = "Wild Glide 2: Jungle Fluzzard Race"
+WILDGLIDESTARG1 = "Wild Glide: Green Star 1"
+WILDGLIDESTARG2 = "Wild Glide: Green Star 2"
+
+# Honeybloom Star Locations
+BEEBLOOMSTAR1 = "Honeybloom 1: Bumble Beginnings"
+BEEBLOOMSTAR2 = "Honeybloom 2: The Secret Wall Jump"
+BEEBLOOMSTARG1 = "Honeybloom: Green Star 1"
+BEEBLOOMSTARG2 = "Honeybloom: Green Star 2"
+
+# Bowser's Lava Lair Star Locations
 LAVALAIRSTAR1 = "Lava Lair 1: Bowser's Big Lava Power Party"
+LAVALAIRSTAR2 = "Lava Lair 2: Lava Lair Speed Run"
+LAVALAIRSTARG1 = "Lava Lair: Green Star 1"
+LAVALAIRSTARG2 = "Lava Lair: Green Star 2"
+
+# World 3
+# Tall Trunk Star Locations
+TALLTREESTAR1 = "Tall Trunk 1: The Flotacious Blimp Fruit"
+TALLTREESTAR2 = "Tall Trunk 2: Tall Trunk's Big Slide"
+TALLTREESTAR3 = "Tall Trunk 3: Tall Trunk's Purple Coin Slide"
+TALLTREESTARG1 = "Tall Trunk: Green Star 1"
+TALLTREESTARG2 = "Tall Trunk: Green Star 2"
+TALLTREESTARG3 = "Tall Trunk: Green Star 3"
+
+# Cloudy Court Star Locations
+CLOUDCOURTSTAR1 = "Cloudy Court 1: Head in the Clouds"
+CLOUDCOURTSTAR2 = "Cloudy Court 2: The Shadow Lining"
+CLOUDCOURTSTAR3 = "Cloudy Court 3: Silver Stars in the Purple Pond"
+CLOUDCOURTSTARG1 = "Cloudy Court: Green Star 1"
+CLOUDCOURTSTARG2 = "Cloudy Court: Green Star 2"
+CLOUDCOURTSTARG3 = "Cloudy Court: Green Star 3"
+
+# Haunty Halls Star Locations
+HAUNTHALLSTAR1 = "Haunty Halls 1: A Glimmer of Bulb Berry"
+HAUNTHALLSTAR2 = "Haunty Halls 2: Sneaking Down the Creepy Corridor"
+HAUNTHALLSTAR3 = "Haunty Halls 3: Spooky Cosmic Clone Chase"
+HAUNTHALLSTARG1 = "Haunty Halls: Green Star 1"
+HAUNTHALLSTARG2 = "Haunty Halls: Green Star 2"
+HAUNTHALLSTARG3 = "Haunty Halls: Green Star 3"
+
+# Freezy Flake Star Locations
+SNOWFLAKESTAR1 = "Freezy Flake 1: Bowser on Ice"
+SNOWFLAKESTAR2 = "Freezy Flake 2: Sorbetti's Chilly Reception"
+SNOWFLAKESTAR3 = "Freezy Flake 3: The Chimp's Skating Challenge"
+SNOWFLAKESTARG1 = "Freezy Flake: Green Star 1"
+SNOWFLAKESTARG2 = "Freezy Flake: Green Star 2"
+SNOWFLAKESTARG3 = "Freezy Flake: Green Star 3"
+
+# Rolling Masterpiece Star Locations
+ROLLMASTERSTAR1 = "Rolling Masterpiece 1: Silver Chomp Grudge Match"
+ROLLMASTERSTAR2 = "Rolling Masterpiece 2: Masterpiece Speed Run"
+ROLLMASTERSTARG1 = "Rolling Masterpiece: Green Star 1"
+ROLLMASTERSTARG2 = "Rolling Masterpiece: Green Star 2"
+
+# Beat Block Star Locations
+BEATBLOCKSTAR1 = "Beat Block 1: Step to the Beep"
+BEATBLOCKSTAR2 = "Beat Block 2: Silver Stars in Double Time"
+BEATBLOCKSTARG1 = "Beat Block: Green Star 1"
+BEATBLOCKSTARG2 = "Beat Block: Green Star 2"
 
 # Bowser Jr.'s Fearsome Fleet
-FEARFLETSTAR1 = "Fearsome Fleet 1: Bowser Jr.'s Mighty Megahammer"
+FEARFLETSTAR1 = "Fearsome Fleet 1: Bowser Jr's Mighty Megahammer"
+FEARFLETSTAR2 = "Fearsome Fleet 2: Megahammer's Daredevil Bash"
+FEARFLETSTARG1 = "Fearsome Fleet: Green Star 1"
+FEARFLETSTARG2 = "Fearsome Fleet: Green Star 2"
 
-# Bowser's Gravity Gauntlet
-GRAVGAUNSTAR1 = "Gravity Gauntlet 1: Breaking into Bowser's Castle"
+# World 4
+# Supermassive Star Locations
+SUPERMASSSTAR1 = "Supermassive 1: Huge Trouble with Big Wigglers"
+SUPERMASSSTAR2 = "Supermassive 2: Big Wigglers Speed Run"
+SUPERMASSSTAR3 = "Supermassive 3: In Full Bloom"
+SUPERMASSSTARG1 = "Supermassive: Green Star 1"
+SUPERMASSSTARG2 = "Supermassive: Green Star 2"
+SUPERMASSSTARG3 = "Supermassive: Green Star 3"
 
-# Bowser Jr.'s Boom Bunker
+# Flipsville Star Locations
+FLIPVILLESTAR1 = "Flipsville 1: Flip-Flopping in Flipsville"
+FLIPVILLESTAR2 = "Flipsville 2: Flipsville's New Digs"
+FLIPVILLESTAR3 = "Flipsville 3: Purple Coin Spin Speed Run"
+FLIPVILLESTARG1 = "Flipsville: Green Star 1"
+FLIPVILLESTARG2 = "Flipsville: Green Star 2"
+FLIPVILLESTARG3 = "Flipsville: Green Star 3"
+
+# Starshine Beach Star Locations
+STARBEACHSTAR1 = "Starshine Beach 1: Surf, Sand, and Silver Stars"
+STARBEACHSTAR2 = "Starshine Beach 2: Climbing the Cloudy Tower"
+STARBEACHSTAR3 = "Starshine Beach 3: Purple Coin Beach Dash"
+STARBEACHSTARG1 = "Starshine Beach: Green Star 1"
+STARBEACHSTARG2 = "Starshine Beach: Green Star 2"
+STARBEACHSTARG3 = "Starshine Beach: Green Star 3"
+
+# Chompworks Star Locations
+CHOMPWORKSTAR1 = "Chompworks 1: Where the Chomps Are Made of Gold"
+CHOMPWORKSTAR2 = "Chompworks 2: Spring Into the Chompworks"
+CHOMPWORKSTAR3 = "Chompworks 3: Cosmic Clones in the Chompworks"
+CHOMPWORKSTARG1 = "Chompworks: Green Star 1"
+CHOMPWORKSTARG2 = "Chompworks: Green Star 2"
+CHOMPWORKSTARG3 = "Chompworks: Green Star 3"
+
+# Honeyhop Star Locations
+HONEYHOPSTAR1 = "Honeyhop 1: The Sweetest Silver Stars"
+HONEYHOPSTAR2 = "Honeyhop 2: The Chimp's Score Challenge"
+HONEYHOPSTARG1 = "Honeyhop: Green Star 1"
+HONEYHOPSTARG2 = "Honeyhop: Green Star 2"
+
+# Sweet Mystery Star Locations
+SWEETMYSTSTAR1 = "Sweet Mystery 1: Bulb Berry's Mysterious Glow"
+SWEETMYSTSTAR2 = "Sweet Mystery 2: Bulb Berry's Purple Coin Glow"
+SWEETMYSTSTARG1 = "Sweet Mystery: Green Star 1"
+SWEETMYSTSTARG2 = "Sweet Mystery: Green Star 2"
+
+# Bowser's Gravity Gauntlet Star Locations
+GRAVGAUNSTAR1 = "Gravity Gauntlet 1: Breaking Into Bowser's Castle"
+GRAVGAUNSTAR2 = "Gravity Gauntlet 2: Gravity Star Speed Run"
+GRAVGAUNSTARG1 = "Gravity Gauntlet: Green Star 1"
+GRAVGAUNSTARG2 = "Gravity Gauntlet: Green Star 2"
+
+# World 5
+# Space Storm Star Locations
+SPACESTORMSTAR1 = "Space Storm 1: Follow Me, Bob-Omb"
+SPACESTORMSTAR2 = "Space Storm 2: To the Top of Topman's Tower"
+SPACESTORMSTAR3 = "Space Storm 3: C'mere, Topman"
+SPACESTORMSTARG1 = "Space Storm: Green Star 1"
+SPACESTORMSTARG2 = "Space Storm: Green Star 2"
+SPACESTORMSTARG3 = "Space Storm: Green Star 3"
+
+# Slipsand Star Locations
+SLIPSANDSTAR1 = "Slipsand 1: Squizzard's Sandy Sinkhole"
+SLIPSANDSTAR2 = "Slipsand 2: Sailing the Sandy Seas"
+SLIPSANDSTAR3 = "Slipsand 3: Squizzard's Daredevil Run"
+SLIPSANDSTARG1 = "Slipsand: Green Star 1"
+SLIPSANDSTARG2 = "Slipsand: Green Star 2"
+SLIPSANDSTARG3 = "Slipsand: Green Star 3"
+
+# Shiverburn Star Locations
+COLDFIRESTAR1 = "Shiverburn 1: Prince Pikante's Peppery Mood"
+COLDFIRESTAR2 = "Shiverburn 2: Octo-Army Icy Rainbow Romp"
+COLDFIRESTAR3 = "Shiverburn 3: The Chimp's Ultimate Skating Challenge"
+COLDFIRESTARG1 = "Shiverburn: Green Star 1"
+COLDFIRESTARG2 = "Shiverburn: Green Star 2"
+COLDFIRESTARG3 = "Shiverburn: Green Star 3"
+
+# Boo Moon Star Locations
+BOOMOONSTAR1 = "Boo Moon 1: Silver Stars Pop-Up"
+BOOMOONSTAR2 = "Boo Moon 2: Haunting the Howling Tower"
+BOOMOONSTAR3 = "Boo Moon 3: The Star in the Sinking Swamp"
+BOOMOONSTARG1 = "Boo Moon: Green Star 1"
+BOOMOONSTARG2 = "Boo Moon: Green Star 2"
+BOOMOONSTARG3 = "Boo Moon: Green Star 3"
+
+# Upside Dizzy Star Locations
+UPDIZZYSTAR1 = "Upside Dizzy 1: A Walk on the Weird Side"
+UPDIZZYSTAR2 = "Upside Dizzy 2: Burning Upside Dizzy"
+UPDIZZYSTARG1 = "Upside Dizzy: Green Star 1"
+UPDIZZYSTARG2 = "Upside Dizzy: Green Star 2"
+
+# Fleet Glide Star Locations
+FLEETFLYSTAR1 = "Fleet Glide 1: Fluzzard's Wild Battlefield Glide"
+FLEETFLYSTAR2 = "Fleet Glide 2: Fastest Feathers in the Galaxy"
+FLEETFLYSTARG1 = "Fleet Glide: Green Star 1"
+FLEETFLYSTARG2 = "Fleet Glide: Green Star 2"
+
+# Bowser Jr.'s Boom Bunker Star Locations
 BOOMBUNKSTAR1 = "Boom Bunker 1: Bowser Jr.'s Boomsday Machine"
+BOOMBUNKSTAR2 = "Boom Bunker 2: Boomsday Machine Daredevil Run"
+BOOMBUNKSTARG1 = "Boom Bunker: Green Star 1"
+BOOMBUNKSTARG2 = "Boom Bunker: Green Star 2"
 
-# Bowser's Galaxy Generator
+# World 6
+# Melty Monster Star Locations
+MELTMONSSTAR1 = "Melty Monster 1: The Magnificent Magma Sea"
+MELTMONSSTAR2 = "Melty Monster 2: A Stroll Down Rolling Lane"
+MELTMONSSTAR3 = "Melty Monster 3: The Chimp's Bowling Challenge"
+MELTMONSSTARG1 = "Melty Monster: Green Star 1"
+MELTMONSSTARG2 = "Melty Monster: Green Star 2"
+MELTMONSSTARG3 = "Melty Monster: Green Star 3"
+
+# Clockwork Ruins Star Locations
+CLOCKRUINSTAR1 = "Clockwork Ruins 1: Time for Adventure"
+CLOCKRUINSTAR2 = "Clockwork Ruins 2: The Adventure of the Purple Coins"
+CLOCKRUINSTAR3 = "Clockwork Ruins 3: The Ledge Hammer Trap"
+CLOCKRUINSTARG1 = "Clockwork Ruins: Green Star 1"
+CLOCKRUINSTARG2 = "Clockwork Ruins: Green Star 2"
+CLOCKRUINSTARG3 = "Clockwork Ruins: Green Star 3"
+
+# Throwback Star Locations
+THROWBACKSTAR1 = "Throwback 1: Return of the Whomp King"
+THROWBACKSTAR2 = "Throwback 2: Silver Stars in the Whomp Fortress"
+THROWBACKSTAR3 = "Throwback 3: Whomp Silver Star Speed Run"
+THROWBACKSTARG1 = "Throwback: Green Star 1"
+THROWBACKSTARG2 = "Throwback: Green Star 2"
+THROWBACKSTARG3 = "Throwback: Green Star 3"
+
+# Battle Belt Star Locations
+BATTBELTSTAR1 = "Battle Belt 1: Mini-Planet Mega-Run"
+BATTBELTSTAR2 = "Battle Belt 2: Mini-Planet Daredevil Run"
+BATTBELTSTAR3 = "Battle Belt 3: Snacktime for Gobblegut"
+BATTBELTSTARG1 = "Battle Belt: Green Star 1"
+BATTBELTSTARG2 = "Battle Belt: Green Star 2"
+BATTBELTSTARG3 = "Battle Belt: Green Star 3"
+
+# Flash Black Star Locations
+FLASHBLACKSTAR1 = "Flash Black 1: Jumping Around in the Dark"
+FLASHBLACKSTAR2 = "Flash Black 2: Dark Octo-Army Romp"
+FLASHBLACKSTARG1 = "Flash Black: Green Star 1"
+FLASHBLACKSTARG2 = "Flash Black: Green Star 2"
+
+# Slimy Spring Star Locations
+SLIMYSPRISTAR1 = "Slimy Spring 1: The Deep Shell Well"
+SLIMYSPRISTAR2 = "Slimy Spring 2: The Chimp's Coin Challenge"
+SLIMYSPRISTARG1 = "Slimy Spring: Green Star 1"
+SLIMYSPIRSTARG2 = "Slimy Spring: Green Star 2"
+
+# Bowser's Galaxy Generator Star Locations
 GALAXYGENSTAR1 = "Galaxy Generator 1: Bowser's Fortified Fortress"
+GALAXYGENSTAR2 = "Galaxy Generator 2: Bowser's Big Bad Speed Run"
+GALAXYGENSTARG1 = "Galaxy Generator: Green Star 1"
+GALAXYGENSTARG2 = "Galaxy Generator: Green Star 2"
+
+# World S
+# Mario Squared Star Locations
+MARIOSQRSTAR1 = "Mario Squared 1: Make Mario a Star"
+MARIOSQRSTAR2 = "Mario Squared 2: Luigi's Purple Coin Chaos"
+MARIOSQRSTARG1 = "Mario Squared: Green Star 1"
+MARIOSQRSTARG2 = "Mario Squared: Green Star 2"
+
+# Rolling Coaster Star Locations
+ROLLSLIDESTAR1 = "Rolling Coaster 1: The Rainbow Road Roll"
+ROLLSLIDESTAR2 = "Rolling Coaster 2: Purple Coins on the Rainbow Road"
+ROLLSLIDESTARG1 = "Rolling Coaster: Green Star 1"
+ROLLSLIDESTARG2 = "Rolling Coaster: Green Star 2"
+
+# Twisty Trials Star Locations
+TWISTTRIALSTAR1 = "Twisty Trials 1: Spinning and Spinning and Spinning"
+TWISTTRIALSTAR2 = "Twisty Trials 2: Turning Turning Double Time"
+TWISTTRIALSTARG1 = "Twisty Trial: Green Star 1"
+TWISTTRIALSTARG2 = "Twisty Trial: Green Star 2"
+
+# Stone Cyclone Star Locations
+STONECYCLOSTAR1 = "Stone Cyclone 1: Silver Stars on the Cyclone"
+STONECYCLOSTAR2 = "Stone Cyclone 2: Tox Box Speed Run"
+STONECYCLOSTARG1 = "Stone Cyclone: Green Star 1"
+STONECYCLOSTARG2 = "Stone Cyclone: Green Star 2"
+
+# Boss Blitz Star Locations
+BOSSBLITSTAR1 = "Boss Blitz 1: Throwback Throwdown"
+BOSSBLITSTAR1 = "Boss Blitz 2: Throwback Throwdown Speed Run"
+BOSSBLITSTARG1 = "Boss Blitz: Green Star 1"
+BOSSBLITSTARG2 = "Boss Blitz: Green Star 2"
+
+# Flip-Out Star Locations
+FLIPOUTSTAR1 = "Flip-Out 1: Wicked Wall Jumps"
+FLIPOUTSTAR2 = "Flip-Out 2: Cosmic Clone Wall Jumpers"
+FLIPOUTSTARG1 = "Flip-Out: Green Star 1"
+FLIPOUTSTARG2 = "Flip-Out: Green Star 2"
 
 # Grandmaster
 GRANDMASTSTAR1 = "Grandmaster 1: The Ultimate Test"
