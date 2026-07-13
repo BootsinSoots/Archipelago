1c395818e73660d3fcd2c4be3166846f58e5341e
carbon-cosmic <corecarboncosmos@gmail.com>
2026-07-12 19:14:55 -0700

    Supermassive regions

--- worlds/smgalaxy2/Rules.py
+++ worlds/smgalaxy2/Rules.py
@@ -289,6 +289,20 @@ def set_rules(world: "SMG2World", player: int): #TODO fix connections
                                                 | (True_()&OptionFilter(WorldShuffle, WorldShuffle.option_Open)))
     # Supermassive
     world.get_region(regname.WORLD4).connect(world.get_region(regname.SUPMASS), "World 4 Slot 1 Galaxy")
+    world.get_region(regname.SUPMASS).connect(world.get_region(regname.SUPMASS1START), "Supermassive Star 1")
+    world.get_region(regname.SUPMASS1START).connect(world.get_region(regname.SUPMASS1PIPES))
+    world.get_region(regname.SUPMASS1PIPES).connect(world.get_region(regname.SUPMASS1THWOMPS))
+    world.get_region(regname.SUPMASS1PIPES).connect.(world.get_region(regname.SUPMASS1GSTAR2))
+    world.get_region(regname.SUPMASS1THWOMPS).connect(world.get_region(regname.SUPMASS1GSTAR2))
+    world.get_region(regname.SUPMASS1THWOMPS).connect(world.get_region(regname.SUPMASS1KOOPAS),
+                                                      "Supermassive 1: Thwomps Launch Star")
+    world.get_region(regname.SUPMASS1KOOPAS).connect(world.get_region(regname.SUPMASS1STARCHIPS),
+                                                     "Supermassive 1: Koopa Launch Star")
+    world.get_region(regname.SUPMASS1STARCHIPS).connect(world.get_region(regname.SUPMASS1GARDEN),
+                                                        "Supermassive 1: ? Block Launch Star")
+    world.get_region(regname.SUPMASS1STARCHIPS).connect(world.get_region(regname.SUPMASS1WIGGLERS))
+    world.get_region(regname.SUPMASS1GARDEN).connect(world.get_region(regname.SUPMASS1WIGGLERS),
+                                                     "Supermassive 1: Garden Planet Launch Star")
     # Sweet Mystery
     world.get_region(regname.WORLD4).connect(world.get_region(regname.SWEETMYS), "World 4 Slot 2 Galaxy")
     # Flipville
