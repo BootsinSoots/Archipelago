c3adfa50d9c87efc669d05e08b140870475671a4
BootsinSoots <theron.swanson@gmail.com>
2026-07-13 23:21:14 -0400

    [Regions] Add Supermassive Regions

--- worlds/smgalaxy2/Rules.py
+++ worlds/smgalaxy2/Rules.py
@@ -417,7 +417,7 @@ def set_rules(world: "SMG2World", player: int): #TODO fix connections
                                                 | (True_()&OptionFilter(WorldShuffle, WorldShuffle.option_Open)))
     # Supermassive
     world.get_region(regname.WORLD4).connect(world.get_region(regname.SUPMASS), "World 4 Slot 1 Galaxy")
-    world.get_region(regname.SUPMASS).connect(world.get_region(regname.SUPMASS1START), "Supermassive Star 1")
+    world.get_region(regname.SUPMASS).connect(world.get_region(regname.SUPMASS1START), "Supermassive Star")
     world.get_region(regname.SUPMASS1START).connect(world.get_region(regname.SUPMASS1PIPES))
     world.get_region(regname.SUPMASS1PIPES).connect(world.get_region(regname.SUPMASS1THWOMPS))
     world.get_region(regname.SUPMASS1PIPES).connect(world.get_region(regname.SUPMASS1GSTAR2))
@@ -428,9 +428,21 @@ def set_rules(world: "SMG2World", player: int): #TODO fix connections
                                                      "Supermassive 1: Koopa Launch Star")
     world.get_region(regname.SUPMASS1STARCHIPS).connect(world.get_region(regname.SUPMASS1GARDEN),
                                                         "Supermassive 1: ? Block Launch Star")
-    world.get_region(regname.SUPMASS1STARCHIPS).connect(world.get_region(regname.SUPMASS1WIGGLERS))
+    world.get_region(regname.SUPMASS1STARCHIPS).connect(world.get_region(regname.SUPMASS1WIGGLERS)) # Trick Entrance
     world.get_region(regname.SUPMASS1GARDEN).connect(world.get_region(regname.SUPMASS1WIGGLERS),
                                                      "Supermassive 1: Garden Planet Launch Star")
+    world.get_region(regname.SUPMASS).connect(world.get_region(regname.SUPMASS1START), "Supermassive Comet Star")
+    world.get_region(regname.SUPMASS2START).connect(world.get_region(regname.SUPMASS2PIPES))
+    world.get_region(regname.SUPMASS2PIPES).connect(world.get_region(regname.SUPMASS2THWOMPS))
+    world.get_region(regname.SUPMASS2THWOMPS).connect(world.get_region(regname.SUPMASS2KOOPAS),
+                                                      "Supermassive Comet: Thwomps Launch Star")
+    world.get_region(regname.SUPMASS2KOOPAS).connect(world.get_region(regname.SUPMASS2STARCHIPS),
+                                                     "Supermassive Comet: Koopa Launch Star")
+    world.get_region(regname.SUPMASS2STARCHIPS).connect(world.get_region(regname.SUPMASS2GARDEN),
+                                                        "Supermassive Comet: ? Block Launch Star")
+    world.get_region(regname.SUPMASS2STARCHIPS).connect(world.get_region(regname.SUPMASS2WIGGLERS)) # Trick Entrance
+    world.get_region(regname.SUPMASS2GARDEN).connect(world.get_region(regname.SUPMASS2WIGGLERS),
+                                                     "Supermassive Comet: Garden Planet Launch Star")
     # Sweet Mystery
     world.get_region(regname.WORLD4).connect(world.get_region(regname.SWEETMYS), "World 4 Slot 2 Galaxy")
     # Flipville
