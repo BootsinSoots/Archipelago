a9db4230a9e2d88c2e79d19c5c9fc2e2bf79f1d9
BootsinSoots <theron.swanson@gmail.com>
2026-08-02 00:24:00 -0400

    [World] Fix potentially missing key in final star blocks option

--- worlds/smgalaxy2/world.py
+++ worlds/smgalaxy2/world.py
@@ -91,6 +91,9 @@ class SMG2World(World):
                 "Clockwork Ruins Coin Luma": self.random.choice(range(random_cap)),
                 "Battle Belt Coin Luma": self.random.choice(range(random_cap))
             }
+        for key in self.options.final_star_blocks.valid_keys:
+            if key not in self.options.final_star_blocks.value.keys():
+                self.options.final_star_blocks.append({key: 0})
         if self.options.goal.value > 2 and self.options.enable_green_stars.value == 2:
             raise OptionError(f"Green Star Locations cannot be locked behind a Galaxy Generator Goal. This error "
                               f"occurred in {self.player_name}'s Super Mario Galaxy 2 world. Their YAML must be fixed")
