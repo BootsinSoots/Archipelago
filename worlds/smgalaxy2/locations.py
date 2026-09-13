6e43a3df1ce780cee04d89b7dfd316a294f90658
BootsinSoots <theron.swanson@gmail.com>
2026-09-13 10:50:54 -0400

    [Locations] Add Star Board location and item+1

--- worlds/smgalaxy2/locations.py
+++ worlds/smgalaxy2/locations.py
@@ -1776,7 +1776,8 @@ special_locations: dict[str, SMG2LocationData] = {
     locname.VISITWORLD7: SMG2LocationData(["Specials"], regname.WORLD7, regname.WORLD7),
     locname.SPEAKTOLUBB: SMG2LocationData(["Specials"], regname.SHIP, regname.SHIP),
     locname.OPENWORLDMP: SMG2LocationData(["Specials"], regname.SHIP, regname.SHIP),
-    locname.STARCTBOARD: SMG2LocationData(["Specials"], regname.SHIP2, regname.SHIP2),
+    locname.STARCTBOARD: SMG2LocationData(["Specials"], regname.SHIP2, regname.SHIP2,
+                                          RB.PowerStarCount(16)),
 }
 
 oneup_locations: dict[str, SMG2LocationData] = {
