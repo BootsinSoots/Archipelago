bc7f282a311e596fe8cb9a4f4e70497fd8858a05
SomeJakeGuy <somejakeguy@gmail.com>
2026-09-16 18:15:14 -0400

    [Client] Boos are now pointers, since the received address could change in future.

--- worlds/luigismansion/Helper_Functions.py
+++ worlds/luigismansion/Helper_Functions.py
@@ -1,4 +1,3 @@
-import copy
 from importlib.resources.abc import Traversable
 from typing import NamedTuple, Optional, TYPE_CHECKING
 import importlib.resources as resources
@@ -194,9 +193,9 @@ class LMDynamicAddresses:
                 case "gcPlayer_Boo_Fields_Archipelago":
                     for boo_idx, boo_name in enumerate(BOO_ITEM_TABLE.keys(), 0):
                         curr_ram_data: LMRamData = ALL_ITEMS_TABLE[boo_name].update_ram_addr[0]
-                        boo_addr = copy.deepcopy(converted_addr) + int(boo_idx/8)
-                        ALL_ITEMS_TABLE[boo_name].update_ram_addr[0] = LMRamData(boo_addr,
-                            curr_ram_data.bit_position, curr_ram_data.ram_byte_size, curr_ram_data.pointer_offset,
+                        boo_ram_offset: int = int(boo_idx/8)
+                        ALL_ITEMS_TABLE[boo_name].update_ram_addr[0] = LMRamData(converted_addr,
+                            curr_ram_data.bit_position, curr_ram_data.ram_byte_size, boo_ram_offset,
                             curr_ram_data.in_game_room_id, curr_ram_data.item_count)
 
                 case "Weapon_Action":
